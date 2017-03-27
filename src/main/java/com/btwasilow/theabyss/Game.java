d5f09a6995df718174d6cbd6a9cef65585db775a
Brent Wasilow <brentwasilow2@gmail.com>
2017-03-27 16:47:09 -0700

    Added comments to Game class.

--- src/main/java/com/btwasilow/theabyss/Game.java
+++ src/main/java/com/btwasilow/theabyss/Game.java
@@ -24,22 +24,45 @@ public class Game implements Runnable {
 	
 	private Level level;
 	
+	/**
+	 * Initializes the high-level game component (i.e., the starting point of
+	 * the game) 
+	 * 
+	 * @param title		name of the game 
+	 * @param width		width of the window
+	 * @param height	height of the window
+	 */
 	public Game(String title, int width, int height) {
 		this.title = title;
 		this.width = width;
 		this.height = height;
 	}
 
+	/**
+	 * Initializes all game components/objects
+	 */
 	private void init() {
 		display = new Display(title, width, height);
-		
 		level = new Level("res/level1.png");
 	}
 	
+	/**
+	 * Performs all update routine checks (i.e., keyboard, physics, graphics)
+	 */
 	private void update() {
+		//*******************//
+		//* Update Routines *//
+		//*******************//
 	}
 	
+	/**
+	 * Performs high-level creation of graphics components. Also delegates
+	 * graphics routine updates using a switch statement (i.e., rendering the
+	 * map, enemies, sprites
+	 */
 	private void render() {
+		// create buffer component for drawing and grab associated
+		// graphics component
 		bs = display.getCanvas().getBufferStrategy();
 		if (bs == null) {
 			display.getCanvas().createBufferStrategy(3);
@@ -47,19 +70,28 @@ public class Game implements Runnable {
 		}
 		g = (Graphics2D) bs.getDrawGraphics();
 
-		// draw here
+		// clear the screen
 		g.setColor(Color.BLACK);
 		g.clearRect(0, 0, Consts.WIDTH, Consts.HEIGHT);
 		
+		//**********************//
+		//* Rendering Routines *//
+		//**********************//
 		MapComponent.render(g, level);
+		
 		
 		bs.show();
 		g.dispose();
 	}
 	
+	/**
+	 * Begins the game loop thread, represented using
+	 * a variable fps but stable update timing
+	 */
 	public void run() {
 		init();
 		
+		// start timing this loop
 		long lastTime = System.nanoTime();
 		long timer = System.currentTimeMillis();
 		double delta = 0;
@@ -67,20 +99,31 @@ public class Game implements Runnable {
 		int updates = 0;
 		
 		while (running) {
+			// request mouse pointer focus
 			display.getCanvas().requestFocus();
 			
+			// check how long our game loop time has elapsed since the "start
+			// timing" portion
 			long now = System.nanoTime();
 			delta += (now - lastTime) / Consts.SKIP_TICKS;
 			lastTime = now;
 			
+			// update as many times as necessary to provide a consistent 
+			// update rate based upon relative elapsed time
+			// (represented through our delta var)
 			while (delta >= 1) {
 				update();
 				updates++;
 				delta--;
 			}
+			
+			// perform unbounded rendering
 			render();
 			frames++;
 			
+			// display our fps and ups (will be removed from production
+			// value code as println() is a costly method across
+			// multiple running threads
 			if ((System.currentTimeMillis() - timer) > 1000) {
 				timer += 1000;
 				System.out.println(updates + " ups | " + frames + " fps");
@@ -90,6 +133,9 @@ public class Game implements Runnable {
 		}
 	}
 	
+	/**
+	 * Accessed by our Main class starting the game thread specifically
+	 */
 	public synchronized void start() {
 		running = true;
 		
@@ -97,6 +143,10 @@ public class Game implements Runnable {
 		thread.start();
 	}
 	
+	/**
+	 * Will be accessed by the Main class to end the game thread safely
+	 *
+	 */
 	public synchronized void stop() {
 		running = false;
 		
